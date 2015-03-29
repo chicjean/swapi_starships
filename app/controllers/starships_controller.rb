be31296307249235414382ee62c7d0f203c12376
Jean Simon <chicjean@gmail.com>
2015-03-29 17:56:47 -0600

    fixed search bug

--- app/controllers/starships_controller.rb
+++ app/controllers/starships_controller.rb
@@ -3,21 +3,21 @@ class StarshipsController < ApplicationController
 	def index
 		@starships = get_starships
 		@class_list = get_class_list
-
+		
 		if params[:name_search] && params[:name_search] != ""
 		 	@starships = StarshipSearch.filter_by(:name, @starships, params[:name_search])
 		end
 
 		if params[:model_search] && params[:model_search] != ""
-			@starships = StarshipSearch.filter_by(:name, @starships, params[:model_search])
+			@starships = StarshipSearch.filter_by(:model, @starships, params[:model_search])
 		end
 		
 		if params[:manufacturer_search] && params[:manufacturer_search] != ""
-			@starships = StarshipSearch.filter_by(:name, @starships, params[:manufacturer_search])
+			@starships = StarshipSearch.filter_by(:manufacturer, @starships, params[:manufacturer_search])
 		end
 
 		if params[:class_search] && params[:class_search] != ""
-		 	@starships = StarshipSearch.filter_by(:name, @starships, params[:class_search])
+		 	@starships = StarshipSearch.filter_by(:class, @starships, params[:class_search])
 		end
 	end
 

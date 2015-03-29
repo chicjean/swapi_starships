d98498315d20528b67bd3469b477c540cf64ae33
Jean Simon <chicjean@gmail.com>
2015-03-29 12:51:08 -0600

    implemented search

--- app/controllers/starships_controller.rb
+++ app/controllers/starships_controller.rb
@@ -3,16 +3,16 @@ class StarshipsController < ApplicationController
 	def index
 		get_starships
 
-		if params[:name_search]
+		if params[:name_search] && params[:name_search] != ""
 		 	@starships = search_ships_by_name(params[:name_search])
 		end
-		# if params[:model_search]
-		# 	@starships = search_ships_by_model(params[:model_search])
-		# end
+		if params[:model_search] && params[:model_search] != ""
+			@starships = search_ships_by_model(params[:model_search])
+		end
 		
-		# if params[:manufacturer_search]
-		# 	@starships = search_ships_by_manufacturer(params[:manufacturer_search])
-		# end
+		if params[:manufacturer_search] && params[:manufacturer_search] != ""
+			@starships = search_ships_by_manufacturer(params[:manufacturer_search])
+		end
 
 		if params[:class_search] && params[:class_search] != ""
 		 	@starships = search_ships_by_class(params[:class_search])
@@ -31,31 +31,40 @@ private
 	end
 
 	def search_ships_by_name(search_params)
-			@starships.select do |ship| 
-				ship.name.downcase.include?(search_params.downcase)
+		search_results = []
+			search_params.split(' ').each do |param|
+				@starships.each do |ship|
+					if ship.name.downcase.include?(param.downcase)
+						search_results << ship
+					end
+				end
 			end
+		search_results.uniq
 	end
 
-		# if search_params.include?(" ")
-		# 	search_params.split(' ').each do |param|
-		# 		@starships.select do |ship|
-		# 			ship.name.downcase.include?(param.downcase)
-		# 			debugger
-		# 		end
-		# 	end
-		# else 
+	def search_ships_by_model(search_params)
+		search_results = []
+			search_params.split(' ').each do |param|
+				@starships.each do |ship|
+					if ship.model.downcase.include?(param.downcase)
+						search_results << ship
+					end
+				end
+			end
+		search_results.uniq
+	end
 
-	# def search_ships_by_model(search_params)
-	# 	@starships.select do |ship| 
-	# 		ship.model.downcase.include?(search_params.downcase) 
-	# 	end
-	# end
-
-	# def search_ships_by_manufacturer(search_params)
-	# 	@starships.select do |ship| 
-	# 		ship.manufacturer.downcase.include?(search_params.downcase)
-	# 	end
-	# end
+	def search_ships_by_manufacturer(search_params)
+		search_results = []
+			search_params.split(' ').each do |param|
+				@starships.each do |ship|
+					if ship.manufacturer.downcase.include?(param.downcase)
+						search_results << ship
+					end
+				end
+			end
+		search_results.uniq
+	end
 
 	def search_ships_by_class(search_params)
 		@starships.select do |ship|

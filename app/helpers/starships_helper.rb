75e882b55bebbfc82cdd446d5e45a1bb898fe6b0
Jean Simon <chicjean@gmail.com>
2015-03-28 23:10:43 -0600

    added atmosphering helper

--- app/helpers/starships_helper.rb
+++ app/helpers/starships_helper.rb
@@ -4,4 +4,12 @@ module StarshipsHelper
 		@starships.length == 0
 	end
 
+	def max_atmosphering?(ship)
+		if ship.max_atmosphering_speed == 'n/a'
+			"This ship is not designed for atmospheric travel."
+		else 
+			"#{ship.max_atmosphering_speed} Meters"
+		end
+	end
+
 end
